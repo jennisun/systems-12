9536399741bd94b6a862fea7006ba2ad658b4400
jennisun <jsun20@stuy.edu>
2021-11-02 22:15:29 -0400

    print out directories

--- main.c
+++ main.c
@@ -11,6 +11,13 @@ int main() {
   stat(".", &s);
   printf("Total Diectory Size: %lld\n", s.st_size);
 
-
+  printf("Directories:\n");
+  struct dirent *entry;
+  entry = readdir(d);
+  while (entry) {
+    if (entry -> d_type == 4) printf("\t%s\n", entry -> d_name);
+    entry = readdir(d);
+  }
+  closedir(d);
 
 }
